0703e7c132c3140b1720ef1fa4eec9836de3956d
qiduuu <qiduuu@gmail.com>
2024-10-04 12:47:19 -0400

    added username

--- pigeonquest.py
+++ pigeonquest.py
@@ -1,1 +1,7 @@
-#test
+
+
+print("You are the last pigeon left.\nYour quest is to defeat the Duck Lord.\n\nYour journey begins now.")
+
+username = input("\nName your pigeon adventurer: ")
+
+print("\n\n\nWelcome, " + username + ", ")
