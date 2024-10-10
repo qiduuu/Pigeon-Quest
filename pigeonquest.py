5c73de792e39b17ca9cfdac3d2637d752b87a31f
qiduuu <qiduuu@gmail.com>
2024-10-10 13:43:47 -0400

    fixed fighting bugs and continued story a bit, added typewriter effect to text

--- pigeonquest.py
+++ pigeonquest.py
@@ -1,3 +1,6 @@
+# for cool typewriter effect on text
+# MUST RUN "pip install pywriter" FOR IT TO WORK
+import pywriter
 
 #time is important for waiting/sleep commands
 import time
@@ -13,7 +16,7 @@ dmg = 10
 '''
 Inventory
 -------------------------------------
-SLOT 1:
+SLOT 1: 
 SLOT 2:
 SLOT 3:
 '''
@@ -55,6 +58,7 @@ class Puffin:
     name = "Puffin"
     xpGain = 50
 
+# checks if user has upgrades and applies them
 def checkUpgrades():
     if hasAtkUp1:
         dmg = 15
@@ -65,60 +69,104 @@ def checkUpgrades():
 
 # Checks for user input on commands from the command variable above such as stats and inventory
 def checkInputs():
-    print("\n-------------------------------------\nCOMMAND LIST\n-------------------------------------\n" + commands + "-------------------------------------")
+    global hp
+    global inBattle
+    global dmg
+    global level
+    global xp
+
+    pywriter.write("\n-------------------------------------\nCOMMAND LIST\n-------------------------------------\n" + commands + "-------------------------------------", rate=0.01)
     userInput = input('\n').lower()
     if userInput == "inv" or userInput.lower() == "inventory":
-        print("\nInventory\n------------------------------------------\nSLOT 1: " + slot1 + "\nSLOT 2: " + slot2 + "\nSLOT 3: " + slot3 + "\n------------------------------------------")
+        pywriter.write("\nInventory\n------------------------------------------\nSLOT 1: " + slot1 + "\nSLOT 2: " + slot2 + "\nSLOT 3: " + slot3 + "\n------------------------------------------", rate=0.01)
         time.sleep(1)
         checkInputs()
     elif userInput == "stats":
-        print("HP: " + str(hp))
-        print("LVL: " + str(level))
-        print("XP: " + str(xp) + "/100")
+        pywriter.write("HP: " + str(hp) + "/100", rate=0.01)
+        pywriter.write("LVL: " + str(level), rate=0.01)
+        pywriter.write("XP: " + str(xp) + "/100", rate=0.01)
         time.sleep(1)
         checkInputs()
-    elif userInput == "attack" or userInput == "atk" or userInput == "hit" or userInput == "stab" and inBattle == True:
+    elif (userInput == "attack" or userInput == "atk" or userInput == "hit" or userInput == "stab") and inBattle == True:
 
-        print("\nYou hit " + Enemy.name + " for " + str(dmg) + " damage!\n")
+        pywriter.write("\nYou hit " + Enemy.name + " for " + str(dmg) + " damage!\n", rate=0.01)
         Enemy.hp = Enemy.hp - dmg
         time.sleep(1)
         if Enemy.hp <= 0:
-            print(Enemy.name + " has been vanquished! Congratulations!\n")
-            print("You gained " + str(Enemy.xpGain) + " XP! :)")
+            pywriter.write(Enemy.name + " has been vanquished! Congratulations!\n", rate=0.01)
+            pywriter.write("You gained " + str(Enemy.xpGain) + " XP! :)", rate=0.01)
             inBattle = False
         else:
-            print(Enemy.name + " is now at " + str(Enemy.hp) + "hp")
+            pywriter.write("\n" + Enemy.name + " is now at " + str(Enemy.hp) + "hp", rate=0.01)
             time.sleep(1)
-            print(Enemy.name + " viciously attacks you, dealing " + str(Enemy.dmg) + " damage!")
-            hp = hp - Enemy.dmg # broken :(
+            pywriter.write("\n" + Enemy.name + " viciously attacks you, dealing " + str(Enemy.dmg) + " damage!", rate=0.01)
+            hp = hp - Enemy.dmg 
             checkInputs()
-    elif userInput == "attack" or userInput == "atk" or userInput == "hit" or userInput == "stab" and inBattle == False:
-        print("You are not in a battle right now. \n\n")
+    elif (userInput == "attack" or userInput == "atk" or userInput == "hit" or userInput == "stab") and inBattle == False:
+        pywriter.write("You are not in a battle right now. \n\n", rate=0.01)
     else:
-        print("Invalid command.\n\n")
+        pywriter.write("Invalid command.\n\n", rate=0.01)
         time.sleep(1)
         checkInputs()
 
 
 
-print("You are the last pigeon left.\nYour quest is to defeat the Duck Lord.\n\nYour journey begins now.")
+pywriter.write("WARNING: DO NOT PRESS ANYTHING WHILE TEXT IS BEING TYPED AS IT MAY MESS UP AREAS REQUIRING INPUT", rate=0.01)
+
+input("\nType anything to continue: ")
+print("\n")
+
+pywriter.write("You are the last pigeon left.\nYour quest is to defeat the Duck Lord.\n\nYour journey begins now.", rate=0.01)
 
 time.sleep(2.5)
 username = input("\nName your pigeon adventurer: ")
-print("\n\nWelcome, " + username + ", to Pigeon Quest.")
+pywriter.write("\n\nWelcome, " + username + ", to Pigeon Quest.", rate=0.01)
 
-# Battle 1
+# Battle 1 Start
 inBattle = True
 bob = Duckling()
 Enemy = bob
 Enemy.name = "Duckling Bob"
 time.sleep(1)
 # fight intro message
-print("\nA little duck jumps out of a nearby bush holding a sword!")
+pywriter.write("\nA little duck jumps out of a nearby bush holding a sword!", rate=0.01)
 time.sleep(2)
-print("\n" + Enemy.name + " challenges you to a duel!\n\nDUCKLING HP: " + str(bob.hp) + "\n\nYOUR HP: " + str(hp))
+pywriter.write("\n" + Enemy.name + " challenges you to a duel!", rate=0.01)
+time.sleep(1)
+pywriter.write("\n\nDUCKLING HP: " + str(bob.hp) + "\n\nYOUR HP: " + str(hp), rate=0.01)
 
 
-
-time.sleep(3)
+time.sleep(2)
 checkInputs()
+# Battle 1 End
+
+time.sleep(1)
+pywriter.write("\n-------------------------------------", rate=0.01)
+
+pywriter.write("\nYou see a village out to the north and begin heading towards it.", rate=0.01)
+pywriter.write("...", rate=0.5)
+pywriter.write("As you approach the village, you notice someone trying to get your attention.", rate=0.01)
+pywriter.write("It's another pigeon! You believed you were the last of your kind, \nhowever this old pigeon man is proof there could be more of you out there!", rate=0.01)
+time.sleep(1.5)
+input("\nType anything to continue: ")
+print("\n")
+pywriter.write("He speaks in a slow, raspy voice\n", rate=0.03)
+pywriter.write("> Old Man Pigeon: Hello, little pigeon.", rate=0.1)
+pywriter.write("> Old Man Pigeon: I've been out that adventurin' business a long long time, boy, \nso I reckon this one's for you", rate=0.1)
+time.sleep(0.5)
+pywriter.write("He hands you an old, torn piece of paper from a quest board.", rate=0.02)
+time.sleep(1)
+print("----------------------------------------------------------------------------------\nADVENTURER REQUIRED!!!!!!\n\nTHE DUCK LORD HAS BECOME TO POWERFUL,\nSOMEBODY MUST DEFEAT HIM!!!")
+time.sleep(0.75)
+input("\nType anything to continue: ")
+print("\n")
+pywriter.write("\n\nREWARD: Honor and Glory!", rate=0.05)
+pywriter.write("\n...", rate=0.175)
+pywriter.write("\n...", rate=0.175)
+pywriter.write("\nand 9 trillion shmillion pigeon dollars", rate=0.03)
+print("----------------------------------------------------------------------------------")
+
+takeQuest = input("\nDo you accept this quest? ('yes' or 'no') ").lower()
+
+if takeQuest != "yes":
+    print("You change your mind and decide to take the quest anyway.")
